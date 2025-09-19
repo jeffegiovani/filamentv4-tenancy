412e36320fa032c67d5a07a665c939772ac319d2
Jefferson Giovani <jeffe.giovani@gmail.com>
2025-09-19 09:53:03 -0300

    Ajuste nas relationships de acordo com a orientação do Dan

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,9 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 
@@ -68,17 +67,19 @@ class Contact extends Model
         );
     }
 
-    public function realEstate(): HasOne
+    public function realEstate(): BelongsTo
     {
-        return $this->hasOne(RealEstate::class, 'id', 'tenant_id');
+        // return $this->hasOne(RealEstate::class, 'id', 'tenant_id');
+        return $this->belongsTo(RealEstate::class, 'tenant_id', 'id');
     }
 
     /**
      * Get Contact's content from ContactType Model
      */
-    public function utmSource(): HasOne
+    public function utmSource(): BelongsTo
     {
-        return $this->hasOne(ContactUtmSource::class, 'id', 'utm_source_id');
+        // return $this->hasOne(ContactUtmSource::class, 'id', 'utm_source_id');
+        return $this->belongsTo(ContactUtmSource::class, 'utm_source_id', 'id');
     }
 
     public function categories(): BelongsToMany
@@ -86,8 +87,4 @@ class Contact extends Model
         return $this->belongsToMany(ContactCategory::class, 'contact_has_contact_category', 'contact_id', 'category_id');
     }
 
-    public function properties(): HasMany
-    {
-        return $this->hasMany(PropertyHasPropertyContact::class, 'contact_id', 'id');
-    }
 }
